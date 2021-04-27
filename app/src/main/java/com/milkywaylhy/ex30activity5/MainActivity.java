90854cf1fcd54510659b60454b681d22b60d472e
하윤 <gkdbs514@naver.com>
2021-04-27 14:50:59 +0900

    Initial commit

---
+++ app/src/main/java/com/milkywaylhy/ex30activity5/MainActivity.java
@@ -0,0 +1,22 @@
+package com.milkywaylhy.ex30activity5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickBtn(View view) {
+        Intent intent= new Intent();
+        intent.setAction("aaa");
+        startActivity(intent);
+    }
+}
